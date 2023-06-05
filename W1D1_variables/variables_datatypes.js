a396d0459873b26bbc735ace6a76094c8503c434
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-06-05 16:07:50 -0600

    init commit and w1d1 variables and data types

---
+++ W1D1_variables/variables_datatypes.js
@@ -0,0 +1,49 @@
+// Variables and data types
+
+// what is a variable?
+// a container
+// 010010001010010101010010110
+// Strings
+var name = "Alice";
+var firstName = 'john';
+var camelCase = "cool";
+console.log(firstName); // "john"
+console.log("hello"); // "hello"
+// Variable names should describe the data that they contain
+// ideally names should be fairly specific
+
+
+var a = "a";
+var b = "b";
+console.log(a+b); // "ab"
+
+console.log(44) // 44
+// numbers
+var pi = 3.14;
+var favNumber = 7777;
+var seatNumber = 13;
+console.log(favNumber) // 7777
+
+var ashujgfahjsjdfkashkashjkfh = 1;
+var THIS_IS_VERY_WEIRD = "yes"
+var num1 = "1990";
+
+
+
+var num2 = 1990;
+num2 = "bob";
+num2 = num1 // ? "1990"
+
+// --- BOOLEAN ---
+
+var vote1 = true;
+var vote2 = false;
+
+// undefined
+// ¯\_(ツ)_/¯
+// Variable with no assigned value
+var cool;
+
+// null
+// Data that is not undefined, but empty. Typically used for variables that are waiting for a value.
+var initialState = null; // this is a comment

26e4eeff87c90df0ab33266c7fc9965c58a6d228
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-06-05 16:13:13 -0600

    refactor and add more context for variables and examples

--- W1D1_variables/variables_datatypes.js
+++ W1D1_variables/variables_datatypes.js
@@ -1,34 +1,53 @@
 // Variables and data types
 
 // what is a variable?
+// Stores data or information
+// Variable names should have some 'common sense principles' in their names to say what the data is.
 // a container
 // 010010001010010101010010110
-// Strings
+
+// ====== data types ==========
+// ----- Strings -----
+// Collection of characters (words)
+
 var name = "Alice";
 var firstName = 'john';
-var camelCase = "cool";
+
+// Naming Conventions
+// how to write the variables
+// JS we use camelCase - the first word is lowercase and all the rest are capital case (the first letter)
+var programmingBasicsStudent = 'Jojo';
+
+var isThisAValidVaraibleName = 'Yes or no?';
+
+var MAN_I_REALLY_LIKE_PANCAKES_AND_TACOS = 'heck yea';
+
+var euihasudfuashdasdashdkslkdgjklsgjklslkdfkldsajfls = 'lol';
+
+var variableNamesDoNotNeedToHaveAnyConventionTheyCanBeOneLongWordThing = 'not good variable name';
+
 console.log(firstName); // "john"
 console.log("hello"); // "hello"
 // Variable names should describe the data that they contain
 // ideally names should be fairly specific
 
-
 var a = "a";
 var b = "b";
-console.log(a+b); // "ab"
+console.log(a+b); // "ab" <- concatenating a string
 
-console.log(44) // 44
-// numbers
+// ----- Numbers -----
+// 12 - Number/integer
+// 0.24 - float/decimal
+
 var pi = 3.14;
 var favNumber = 7777;
 var seatNumber = 13;
 console.log(favNumber) // 7777
+console.log(44) // 44
 
 var ashujgfahjsjdfkashkashjkfh = 1;
 var THIS_IS_VERY_WEIRD = "yes"
 var num1 = "1990";
-
-
 
 var num2 = 1990;
 num2 = "bob";
@@ -47,3 +66,4 @@ var cool;
 // null
 // Data that is not undefined, but empty. Typically used for variables that are waiting for a value.
 var initialState = null; // this is a comment
+var x = '';
